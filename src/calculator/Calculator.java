ead845aa6751f3cf3612455d87e43a0ae4405279
andrew <andrewpalmer51@gmail.com>
2019-10-15 16:34:59 -0400

    Add comments

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,13 +1,27 @@
+/*
+ * File name: Calculator
+ * Author: Andrew Palmer
+ * Course: CST8221 - JAP 302
+ * Assignment: 1
+ * Date: 2019-10-15
+ * Professor: Svillen Ranev
+ * Purpose: The main for the calculator application
+ */
 package calculator;
 
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * The calculator class contains the main function for the application.
+ */
 public class Calculator {
 
     public static void main(String[] args) {
-        CalculatorSplashScreen splashScreen = new CalculatorSplashScreen(5000);
+        CalculatorSplashScreen splashScreen = new CalculatorSplashScreen(5000); // create new splash screen object with 5 sec timer
         splashScreen.showSplashWindow();
+
+        // invoke a new runnable instance of Calculator after the splash screen finishes
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
--- src/calculator/CalculatorSplashScreen.java
+++ src/calculator/CalculatorSplashScreen.java
@@ -1,47 +1,65 @@
+/*
+ * File name: CalculatorSplashScreen
+ * Author: Andrew Palmer
+ * Course: CST8221 - JAP 302
+ * Assignment: 1
+ * Date: 2019-10-15
+ * Professor: Svillen Ranev
+ * Purpose: Creates a splash screen for the calculator application
+ */
 package calculator;
 
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Author: Andrew Palmer
+ * Version: 1.0
+ * See: calculator
+ * Since: 1.8.222
+ */
 public class CalculatorSplashScreen extends JWindow {
 
     /**
      * Swing components are serializable and require serialVersionUID
      */
     private static final long serialVersionUID = 6248477390124803341L;
+
     /**
      * Splash screen duration
      */
     private final int duration;
 
+    /**
+     * The constructor for a new splash screen object
+     * @param duration the duration that the splash screen lasts
+     */
     protected CalculatorSplashScreen(int duration) {
         this.duration = duration;
     }
 
+    /**
+     * Launches and sets the properties of the splash screen object
+     */
     protected void showSplashWindow() {
-        //create content pane
-        JPanel content = new JPanel(new BorderLayout());
-        // or use the window content pane
-        //  JPanel content = (JPanel)getContentPane();
+
+        JPanel content = new JPanel(new BorderLayout());//create content pane
         content.setBackground(Color.BLACK);
 
-        // Set the window's bounds, position the window in the center of the screen
-        int width = 700;
-        int height = 500;
+        int width = 700; // the width of the splash screen
+        int height = 500; // the height of the splash screen
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (screen.width - width) / 2;
-        int y = (screen.height - height) / 2;
-        //set the location and the size of the window
-        setBounds(x, y, width, height);
+        int x = (screen.width - width) / 2;  // calculate the x value for the position
+        int y = (screen.height - height) / 2; // calculate the y value for the position
+        setBounds(x, y, width, height); // set the bounds of the splash screen
 
-        JLabel label = new JLabel(new ImageIcon(getClass().getResource("splash.jpeg")));
+        JLabel label = new JLabel(new ImageIcon(getClass().getResource("splash.jpeg"))); // gets the picture for the splash screen
 
-        JLabel demo = new JLabel("Andrew Palmer - 040719945", JLabel.LEFT);
+        JLabel demo = new JLabel("Andrew Palmer - 040719945", JLabel.LEFT); // sets the label for the bottom of the splash screen
         demo.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 14));
         demo.setForeground(Color.WHITE);
         content.add(label, BorderLayout.CENTER);
         content.add(demo, BorderLayout.SOUTH);
-        // create custom RGB color
 
         content.setBorder(BorderFactory.createLineBorder(Color.BLACK, 10));
 
